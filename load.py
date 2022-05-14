f0c8d8d1a989917691d565093c5e983ea706e1aa
yqy1277 <1277683944@qq.com>
2022-05-15 02:38:49 +0800

    Try to pack the function of reading the data.

--- load.py
+++ load.py
@@ -1,12 +1,18 @@
-
 import numpy as np
 import torch
+from torch_geometric.data import Data
 
-input_data = np.load(r"./data/hh_graph.npy")
-data = input_data.tolist()
 
-hh_edge_index = torch.tensor(data, dtype=torch.long) - 390  # 边索引需要减去390
-hh_x = torch.tensor([[i] for i in range(390, 1195)])
+#
+# input_data = np.load(r"./data/ss_graph.npy")
+# data = input_data.tolist()
 
-print(hh_x)
-
+def load_data(path, dim):
+    sh_edge = np.load(path)
+    sh_edge = sh_edge.tolist()
+    sh_edge_index = torch.tensor(sh_edge, dtype=torch.long)
+    sh_x = torch.tensor([[i] for i in range(dim, )], dtype=torch.float)
+    sh_data = Data(x=sh_x, edge_index=sh_edge_index.t().contiguous())  ### 制图
+    # sh_data_adj = SparseTensor(row=sh_data.edge_index[0], col=sh_data.edge_index[1],
+    #                            sparse_sizes=(1195, 1195))  ### 邻接矩阵
+    return sh_data
